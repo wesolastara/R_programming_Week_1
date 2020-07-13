a0f5d61c13c6cc10ea7e07080700cb23c236c14e
wesolastara <maciejgielnik@gmail.com>
2020-07-13 19:21:28 +0200

    xD

--- Week1.R
+++ Week1.R
@@ -17,6 +17,9 @@ my_div <- z/my_sqrt
 my_div
 
 
+## abs()   - wartość bezwzględna
+
+
 c(1, 2, 3, 4) + c(0, 10)
 
 c(1, 2, 3, 4) + c(0, 10, 100)
@@ -26,5 +29,57 @@ z*2+1000
 my_div
 
 ## Lesson 2
+
+getwd()        ## working directory
+ls()           ## lista obiektów
+
+x  <-   9
+
+dir()          ## lista plików
+list.files()   ## lista plików
+
+?list.files
+
+args(list.files)
+
+old.dir <- getwd()
+
+dir.create("testdir")     ## tworzenie katalogu
+
+setwd("testdir")       ## set working directory
+
+file.create("mytest.R")   ## tworzenie pliku
+
+dir()
+
+file.exists("mytest.R")    ## sprawdza czy plik istnieje
+
+file.info("mytest.R")      ## wyświetla info o pliku
+
+?file.rename
+
+file.rename("mytest.R", "mytest2.R")    ## rename
+
+file.remove("mytest.R")             ## remove
+
+
+?file.copy                          ## kopiowanie
+file.copy("mytest2.R", "mytest3.R") ## kopiowanie
+
+file.path("mytest3.R")
+?file.path
+file.path("folder1", "folder2")    ## ustawianie ścierzek
+
+dir.create(file.path('testdir2', 'testdir3'), recursive = TRUE)    ## tworzenie katalogów rekurencyjnych
+
+setwd(old.dir)
+
+
+?file.remove
+?unlink
+
+
+unlink("testdir", recursive = T)   ## usuwanie katalogów
+
 
 
