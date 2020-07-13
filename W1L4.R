f459c52d94f28f8e82c80fe8a327b3fa95d47575
wesolastara <maciejgielnik@gmail.com>
2020-07-13 20:23:43 +0200

    W1L4

---
+++ W1L4.R
@@ -0,0 +1,24 @@
+swirl()
+num_vect <- c(0.5, 55, -10, 6)
+tf <- num_vect <1
+tf
+num_vect >= 6
+
+## !=   inequality
+##   |  lub
+##   &   i
+## !A   nie A
+
+
+my_char <- c("My", "name", "is")
+my_char
+
+paste(my_char, collapse = " ")
+my_name <- c(my_char, "Maciej")
+my_name
+paste(my_name, collapse = " ")
+
+paste("Hello", "world!", sep = " ")
+
+paste(1:3, c("X", "Y", "Z"), sep = "")
+paste(LETTERS, 1:4, sep = "-")
