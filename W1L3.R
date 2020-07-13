d674548d9c4e68b5b4a005e70c0880e213fc5fc7
wesolastara <maciejgielnik@gmail.com>
2020-07-13 20:00:17 +0200

    W1L3

---
+++ W1L3.R
@@ -0,0 +1,22 @@
+swirl()
+1:20
+pi:10
+
+15:1
+
+
+?`:`
+
+seq(1, 20)
+seq(0, 10, by = 0.5)
+my_seq <- seq(5, 10, length=30)
+length(my_seq)
+1:length(my_seq)
+seq(along.with = my_seq)
+?seq
+seq_along(my_seq)
+
+
+rep(0, times = 40)
+rep(c(0, 1, 2), times = 10)
+rep(c(0, 1, 2), each = 10)
